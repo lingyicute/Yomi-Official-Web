a806bb9fe62777b696fff6ab67af0435dc61fac1
lingyicute <160479206+lingyicute@users.noreply.github.com>
2025-01-11 18:17:11 +0800

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,10 @@
-import createIntlMiddleware from 'next-intl/middleware';
-import {localeConfig} from "@/i18n";
+import createMiddleware from 'next-intl/middleware';
+import {localeConfig} from "@/i18n/config";
 
-export default createIntlMiddleware(localeConfig);
+export default createMiddleware({
+    locales: localeConfig.locales,
+    defaultLocale: localeConfig.defaultLocale
+});
 
 export const config = {
     matcher: ['/((?!api|_next|favicon.ico|images).*)']
